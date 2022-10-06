e25bcc76ab5128e0699826cc60830c20755be1d3
GyZsofi <zsofia.gyori00@gmail.com>
2022-10-06 12:26:05 +0200

    list the best rated books

--- src/index.js
+++ src/index.js
@@ -7,6 +7,24 @@ for (let index = 0; index < 30; index++) {
     rnumber = Math.floor(Math.random() * 10 + 1);
     list.push(new book_1.Book("Book#" + index, rnumber));
 }
-for (let index = 0; index < list.length; index++) {
+/*for (let index = 0; index < list.length; index++) {
     console.log(list[index]);
 }
+
+for (let index = 0; index < list.length; index++) {
+    if(list[index].rating > 7){
+        console.log(list[index]);
+    }
+}*/
+function bestof(lista) {
+    var bestRating = 0;
+    var bestIndex = 0;
+    for (let index = 0; index < lista.length; index++) {
+        if (lista[index].rating > bestRating) {
+            bestRating = lista[index].rating;
+            bestIndex = index;
+        }
+    }
+    console.log(lista[bestIndex]);
+}
+bestof(list);
--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,22 @@ for (let index = 0; index < 30; index++) {
 for (let index = 0; index < list.length; index++) {
     console.log(list[index]);
 }
+
+for (let index = 0; index < list.length; index++) {
+    if(list[index].rating > 7){
+        console.log(list[index]);
+    }
+}
+
+function bestof(lista: Array<Book>){
+    var bestRating = 0;
+    var bestIndex = 0;
+    for (let index = 0; index < lista.length; index++) {
+        if(lista[index].rating > bestRating){
+            bestRating = lista[index].rating;
+            bestIndex = index;
+        }
+    }
+    console.log(lista[bestIndex]);
+}
+bestof(list)
